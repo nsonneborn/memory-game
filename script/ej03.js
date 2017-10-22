b4386fa484a6c9fbab3751be2c146f5971a70e09
Nina Sonneborn <nsonneborn@middlebury.edu>
2017-10-22 13:13:29 +0200

    resize and replace images

--- script/ej03.js
+++ script/ej03.js
@@ -6,34 +6,35 @@ var ImgFound = 0;
 var Source = "#boxcard";
 
 var ImgSource = [
-  "http://img5.uploadhouse.com/fileuploads/17699/176992640c06707c66a5c0b08a2549c69745dc2c.png",
-  "http://img6.uploadhouse.com/fileuploads/17699/17699263b01721074bf094aa3bc695aa19c8d573.png",
-  "http://img6.uploadhouse.com/fileuploads/17699/17699262833250fa3063b708c41042005fda437d.png",
-  "http://img9.uploadhouse.com/fileuploads/17699/176992615db99bb0fd652a2e6041388b2839a634.png",
-  "http://img4.uploadhouse.com/fileuploads/17699/176992601ca0f28ba4a8f7b41f99ee026d7aaed8.png",
-  "http://img3.uploadhouse.com/fileuploads/17699/17699259cb2d70c6882adc285ab8d519658b5dd7.png",
-  "http://img2.uploadhouse.com/fileuploads/17699/1769925824ea93cbb77ba9e95c1a4cec7f89b80c.png",
-  "http://img7.uploadhouse.com/fileuploads/17699/1769925708af4fb3c954b1d856da1f4d4dcd548a.png",
-  "http://img9.uploadhouse.com/fileuploads/17699/176992568b759acd78f7cbe98b6e4a7baa90e717.png",
-  "http://img9.uploadhouse.com/fileuploads/17699/176992554c2ca340cc2ea8c0606ecd320824756e.png"
+  "images/ada-lovelace.jpg",
+  "images/ellen-ochoa.jpg",
+  "images/grace-hopper.png",
+  "images/katherine-johnson.jpg",
+  "images/marie-curie.jpg",
+  "images/rosalind-franklin.png",
+  "images/sophie-germain.jpg",
+  "images/women-eniac.jpg",
+  "images/women-in-tech.jpg",
+  "images/WomenSTEM.jpg"
 ];
 
 function RandomFunction(MaxValue, MinValue) {
 		return Math.round(Math.random() * (MaxValue - MinValue) + MinValue);
 	}
-	
+
 function ShuffleImages() {
 	var ImgAll = $(Source).children();
 	var ImgThis = $(Source + " div:first-child");
 	var ImgArr = new Array();
 
+
 	for (var i = 0; i < ImgAll.length; i++) {
 		ImgArr[i] = $("#" + ImgThis.attr("id") + " img").attr("src");
 		ImgThis = ImgThis.next();
 	}
-	
+
 		ImgThis = $(Source + " div:first-child");
-	
+
 	for (var z = 0; z < ImgAll.length; z++) {
 	var RandomNumber = RandomFunction(0, ImgArr.length - 1);
 
@@ -61,7 +62,7 @@ function OpenCard() {
 
 	if ($("#" + id + " img").is(":hidden")) {
 		$(Source + " div").unbind("click", OpenCard);
-	
+
 		$("#" + id + " img").slideDown('fast');
 
 		if (ImgOpened == "") {
